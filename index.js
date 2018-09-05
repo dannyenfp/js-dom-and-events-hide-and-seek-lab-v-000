3c77c3547cc679a6db2f9bc15fd395d72335007d
Daniel Valmas <daniel.valmas@gmail.com>
2018-09-05 18:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ for (let i = 0; i < rankedLists.length; i++) {
 
 function deepestChild() {
   const grandNode = document.querySelector('#grand-node');
-  return grandNode;
+  return grandNode.children;
 }
