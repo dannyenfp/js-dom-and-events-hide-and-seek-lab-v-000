f28120aa12f9a461dc3940d70042e7b147fc0fc4
Daniel Valmas <daniel.valmas@gmail.com>
2018-09-05 17:43:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let num = parseInt(n);
+  
+  
+}
+
+function deepestChild() {
+  
+}
